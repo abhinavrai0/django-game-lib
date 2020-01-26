e060ee7f393df738870144fbd87f96407b373e2a
Abhinav Rai <arai@ncsu.edu>
2020-01-26 18:24:12 -0500

    Updated admin display models for multi fields models

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,8 +4,21 @@ from django.contrib import admin
 
 from .models import Genre, Game, Gamer, Publisher, Purchases
 
-admin.site.register(Game)
-admin.site.register(Gamer)
-admin.site.register(Purchases)
+#admin.site.register(Game)
+#admin.site.register(Gamer)
+#admin.site.register(Purchases)
 admin.site.register(Publisher)
 admin.site.register(Genre)
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'genre', 'about')
+
+@admin.register(Gamer)
+class GamerAdmin(admin.ModelAdmin):
+    list_display = ('email', 'name')
+
+
+@admin.register(Purchases)
+class PurchasesAdmin(admin.ModelAdmin):
+    list_display = ('gamer', 'game', 'date_purchased', 'price', 'review')
