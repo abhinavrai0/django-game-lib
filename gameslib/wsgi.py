a610bf1f0ec219195ed735ad739d3372e0b1222e
Abhinav Rai <arai@ncsu.edu>
2020-01-26 15:16:32 -0500

    Initial commit

---
+++ gameslib/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for gameslib project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gameslib.settings')
+
+application = get_wsgi_application()
