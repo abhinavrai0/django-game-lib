af099838c20a2c44cff6372ae7e7f8d4ae1acda7
Abhinav Rai <arai@ncsu.edu>
2020-01-26 21:48:35 -0500

    Adding index template...in progress

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from catalog.models import Game, Gamer, Publisher, Purchases, Genre
 
 # Create your views here.
+
+
+def index(request):
+    num_games = Game.objects.all().count()
+    num_purchases = Purchases.objects.all().count()
+    num_publishers = Publisher.objects.all().count()
+    num_gamers = Gamer.objects().all().count()
+    context = {
+        'num_games':  num_games,
+        'num_purchases': num_purchases,
+        'num_publishers': num_publishers,
+        'num_gamers': num_gamers,
+    }
+    return render(request, 'index.html', context=context)
+
